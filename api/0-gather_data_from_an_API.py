206eeea1c336d1d9445c31a66359a71d737d8a2d
alustudent <s.rurangami@alustudent.com>
2025-02-14 09:28:57 +0200

    Solve blank line

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -12,39 +12,42 @@ Arguments:
 Needs:
     - The requests module
 """
+
 import requests
 import sys
 
-def fetch_todo_list(employee_id):
 
-    
+def fetch_todo_list(employee_id):
+    """Fetch and display an employee's TODO list progress."""
     url_user = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     url_todos = (
         f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
     )
-    
+
     user_response = requests.get(url_user)
     todos_response = requests.get(url_todos)
-    
+
     user = user_response.json()
     todos = todos_response.json()
-    
+
     employee_name = user.get("name")
     total_tasks = len(todos)
     done_tasks = [task for task in todos if task.get("completed")]
     number_of_done_tasks = len(done_tasks)
-    
+
     print(
-        f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):"
+        f"Employee {employee_name} is done with tasks "
+        f"({number_of_done_tasks}/{total_tasks}):"
     )
     for task in done_tasks:
         print(f"\t {task.get('title')}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python3 script.py <employee_id>")
         sys.exit(1)
-    
+
     try:
         employee_id = int(sys.argv[1])
         fetch_todo_list(employee_id)
