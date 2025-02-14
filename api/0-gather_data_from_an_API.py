9e90c5a301a8498b7ec23ee8513ec1a4b5fcc170
alustudent <s.rurangami@alustudent.com>
2025-02-14 09:01:49 +0200

    Change code

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,31 +1,38 @@
- #!/usr/bin/python3
-
+#!/usr/bin/python3
 import requests
 import sys
 
+def fetch_todo_list(employee_id):
+    url_user = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    url_todos = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    
+    user_response = requests.get(url_user)
+    todos_response = requests.get(url_todos)
+    
+    if user_response.status_code != 200 or todos_response.status_code != 200:
+        print("Error: Unable to fetch data")
+        return
+    
+    user = user_response.json()
+    todos = todos_response.json()
+    
+    employee_name = user.get("name")
+    total_tasks = len(todos)
+    done_tasks = [task for task in todos if task.get("completed")]
+    number_of_done_tasks = len(done_tasks)
+    
+    print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
+    for task in done_tasks:
+        print(f"\t {task.get('title')}")
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
+        print("Usage: python3 script.py <employee_id>")
         sys.exit(1)
     
-    employee_id = sys.argv[1]
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-    
-    user_response = requests.get(user_url)
-    if user_response.status_code != 200:
-        print(f"Error: User with ID {employee_id} not found.")
+    try:
+        employee_id = int(sys.argv[1])
+        fetch_todo_list(employee_id)
+    except ValueError:
+        print("Error: Employee ID must be an integer")
         sys.exit(1)
-    user_data = user_response.json()
-    employee_name = user_data.get('name')
-    
-    todos_response = requests.get(todos_url)
-    todos_data = todos_response.json()
-    
-    total_tasks = len(todos_data)
-    completed_tasks = [todo for todo in todos_data if todo['completed']]
-    num_completed = len(completed_tasks)
-    
-    print(f"Employee {employee_name} is done with tasks({num_completed}/{total_tasks}):")
-    for task in completed_tasks:
-        print(f"\t {task['title']}")
