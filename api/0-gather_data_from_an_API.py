2ba2cd8c1c530ea52772e3248cdca98a6e339993
alustudent <s.rurangami@alustudent.com>
2025-02-14 09:22:16 +0200

    solve white space

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -17,14 +17,12 @@ import sys
 
 def fetch_todo_list(employee_id):
     url_user = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    url_todos = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    url_todos = (
+        f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    )
     
     user_response = requests.get(url_user)
     todos_response = requests.get(url_todos)
-    
-    if user_response.status_code != 200 or todos_response.status_code != 200:
-        print("Error: Unable to fetch data")
-        return
     
     user = user_response.json()
     todos = todos_response.json()
@@ -34,7 +32,9 @@ def fetch_todo_list(employee_id):
     done_tasks = [task for task in todos if task.get("completed")]
     number_of_done_tasks = len(done_tasks)
     
-    print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
+    print(
+        f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):"
+    )
     for task in done_tasks:
         print(f"\t {task.get('title')}")
 
